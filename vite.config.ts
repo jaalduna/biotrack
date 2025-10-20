94eda2e0949d3f290aa87fb476d11fc5a8d6724c
Jaalduna <jaalduna@gmail.com>
2025-10-20 09:54:14 -0300

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/biotrack",
+  base: "/biotrack/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
