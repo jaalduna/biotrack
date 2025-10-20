26d036ebc4eb6a8a02aa1a78ef69e325261db351
Jaalduna <jaalduna@gmail.com>
2025-10-20 00:40:47 -0300

    add 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HashRouter, RouterProvider } from "react-router"; // Changed
+import { RouterProvider } from "react-router"; // Changed
 import { router } from "./router/router.app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <RouterProvider router={router} />
-    </HashRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
--- src/router/router.app.tsx
+++ src/router/router.app.tsx
@@ -3,14 +3,17 @@ import { PatientDetailPage } from "@/pages/PatientsDetailPage";
 import { PatientsPage } from "@/pages/PatientsPage";
 import { createBrowserRouter } from "react-router";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <BioTrack />,
-  },
-  { path: "/patients", element: <PatientsPage /> },
-  {
-    path: "/patients/:id",
-    element: <PatientDetailPage />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <BioTrack />,
+    },
+    { path: "/patients", element: <PatientsPage /> },
+    {
+      path: "/patients/:id",
+      element: <PatientDetailPage />,
+    },
+  ],
+  { basename: "/biotrack" },
+);
