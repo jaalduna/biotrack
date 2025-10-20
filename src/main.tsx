7277214da3d9de47bffcc7456d48091029ca25bc
Jaalduna <jaalduna@gmail.com>
2025-10-20 00:30:39 -0300

    add deploy yml

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router";
-
+import { HashRouter, RouterProvider } from "react-router"; // Changed
 import { router } from "./router/router.app";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <RouterProvider router={router} />
+    </HashRouter>
   </StrictMode>,
 );
